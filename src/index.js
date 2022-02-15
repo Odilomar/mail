c70c3d1ef232e86763c9a368e09b764824ac534b
Odilomar Júnior <odilomar.junior@gmail.com>
2022-02-15 19:53:23 -0300

    send email with attachments by buffer

--- src/index.js
+++ src/index.js
@@ -2,24 +2,47 @@ const path = require("path");
 const fs = require("fs").promises;
 const { sendEmail } = require('./mail')
 
-const main = async () => {
-  const dirname = path.join(__dirname, "archive");
-  const files = await fs.readdir(dirname);
+const dirname = path.join(__dirname, "archive");
 
-  const options = {
-    to: "odilomar.junior@gmail.com",
-    subject: "Hello ✔",
-    text: "Hello world?",
-    html: "<b>Hello world?</b>",
-    attachments: files.map((file) => {
-      return {
-        filename: file,
-        path: path.join(dirname, file),
-      };
-    }),
-  };
-
-  await sendEmail(options);
+const options = {
+  to: "odilomar.junior@gmail.com",
+  subject: "Hello ✔",
+  text: "Hello world?",
+  html: "<b>Hello world?</b>",
 };
 
-main();
+const sendEmailtWithAttachmentsByPath = async () => {
+  const files = await fs.readdir(dirname)
+  options.subject= "Hello (attachments by path) ✔"
+
+  options.attachments = files.map((file) => {
+    return {
+      filename: file,
+      path: path.join(dirname, file),
+    };
+  }),
+
+  await sendEmail(options)
+};
+
+const sendEmailtWithAttachmentsByBufer = async () => {
+  const files = await fs.readdir(dirname)
+
+  options.attachments = []
+  options.subject= "Hello (attachments by bufer) ✔"
+
+  for (const file of files) {
+    const filePath = path.join(dirname, file)
+    const buffer = await fs.readFile(filePath, { encoding: 'utf-8' })
+
+    options.attachments.push({      
+      filename: file,
+      content: buffer,
+    })
+  }
+
+  await sendEmail(options)
+}
+
+// sendEmailtWithAttachmentsByPath();
+// sendEmailtWithAttachmentsByBufer();
